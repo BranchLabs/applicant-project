a86c781827003800cceed1d6bd0dc31d9ca06593
Awoyo Oluwatoyin <awoyotoyin@gmail.com>
2017-02-03 01:30:59 +0100

    added db configuration file

---
+++ Config.php
@@ -0,0 +1,18 @@
+<?php
+
+/*
+ * BranchLabs Applicant Project
+ */
+
+/**
+ * Description of Config
+ *
+ * @author Awoyo Oluwatoyin
+ */
+class Config
+{
+    const DB_HOST = 'localhost';
+    const DB_USER = 'root';
+    const DB_PASS = 'p455w0rd';
+    const DB_NAME = 'branchlabs';
+}
