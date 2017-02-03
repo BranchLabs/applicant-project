220593e4ac99253402eb77b3e6567f4236ddfc3e
Awoyo Oluwatoyin <awoyotoyin@gmail.com>
2017-02-03 06:57:03 +0100

    changed db credentials

--- yii2-mvc/config/db.php
+++ yii2-mvc/config/db.php
@@ -2,8 +2,8 @@
 
 return [
     'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=localhost;dbname=yii2basic',
+    'dsn' => 'mysql:host=localhost;dbname=branchlabs_yii2',
     'username' => 'root',
-    'password' => '',
+    'password' => 'p455w0rd',
     'charset' => 'utf8',
 ];
